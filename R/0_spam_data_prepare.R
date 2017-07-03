adf62a407d8f8560081aeaa598d0f838b1ba5d05
kiendang <trungkiendang@hotmail.com>
2017-07-03 23:26:49 +0800

    Better practice

--- R/0_spam_data_prepare.R
+++ R/0_spam_data_prepare.R
@@ -1,6 +1,6 @@
 train.ratio = 0.9
 
-spam <- read.csv("./data/spambase.data", header=F)
+spam <- read.csv("./data/spambase.data", header=FALSE)
 data <- spam[, -c(55, 56, 57)]
 
 set.seed(1)
@@ -9,5 +9,5 @@ train.index <- sample(1:dim(data)[1], as.integer(train.ratio * dim(data)[1]))
 training <- data[train.index, ]
 testing <- data[-train.index, ]
 
-write.table(training, "./data/training", quote=F, row.names=F, col.names=F)
-write.table(testing, "./data/testing", quote=F, row.names=F, col.names=F)
+write.table(training, "./data/training", quote=FALSE, row.names=FALSE, col.names=FALSE)
+write.table(testing, "./data/testing", quote=FALSE, row.names=FALSE, col.names=FALSE)
